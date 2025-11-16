368ba7ffbe59313ab93e8baf21e35c63db3f06b7
VivianMoonlight <vivianbc@proton.me>
2025-11-17 01:00:42 +0800

    rename folder and add a loader

---
+++ PortableWardrobeLoader.js
@@ -0,0 +1,18 @@
+// ==UserScript==
+// @name         Vivians-Portable-Wardrobe
+// @namespace    https://www.bondageprojects.com/
+// @version      0.1
+// @description  Loader for Portable Wardrobe
+// @author       VivianMoonlight
+// @match        https://bondageprojects.elementfx.com/*
+// @match        https://www.bondageprojects.elementfx.com/*
+// @match        https://bondage-europe.com/*
+// @match        https://www.bondage-europe.com/*
+// @match        https://bondage-asia.com/*
+// @match        https://www.bondage-asia.com/*
+// @match        http://localhost:*/*
+// @run-at       document-end
+// @grant        none
+// ==/UserScript==
+
+import(`https://VivianMoonlight.github.io/Vivians-Portable-Wardrobe/PortableWardrobe/init.js${Date.now()}`);
