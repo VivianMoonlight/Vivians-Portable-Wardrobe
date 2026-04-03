39a29286baa5509da92697c2f76afe90414358e5
VivianMoonlight <vivianbc@proton.me>
2026-04-03 20:49:16 +0800

    beta docs update

--- ViviansPortableWardrobeLoader.user.js
+++ ViviansPortableWardrobeLoader.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Vivians-Portable-Wardrobe
 // @namespace    https://www.bondageprojects.com/
-// @version      0.10.1-beta.3
+// @version      0.10.1-beta.4
 // @description  Loader for Portable Wardrobe
 // @author       VivianMoonlight
 // @match        https://bondageprojects.elementfx.com/*
